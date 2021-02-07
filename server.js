f488c2cf7e0f899f66c68adc67928de28a982033
Julie Altman <jtskolnik@gmail.com>
2021-02-07 12:25:13 -0500

    api calls functionning on backend

--- server.js
+++ server.js
@@ -8,15 +8,26 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theresfoodathome");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theresfoodathome", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 // Start the API server
 app.listen(PORT, function() {
