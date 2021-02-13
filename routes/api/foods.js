e2645829d20f83d36b877ae3fb5c01adecf82c3b
Ness <johnsondavid1107@gmail.com>
2021-02-13 13:34:55 -0500

    cleaned up seed and added seed, also changed the route location

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const FoodsController = require("../../controllers/foodsController");
+const db = require("../../models")
 
 // Matches with "/api/foods"
 router.route("/")
@@ -9,4 +10,25 @@ router.route("/recipes/:ingredient")
     .get(FoodsController.findBySearch);
 
 
+
+
+router.post("/addFood", function (req, res) {
+    //the fireBaseId number needs to be a current 
+    db.User.findOneAndUpdate({ fireBaseId: 1 }, {
+        $push: {
+            foodItem: {
+                name: "Stew",
+                dateOfPurchase: null,
+                daysFresh: 5,
+                spoiled: false,
+                location: "pantry"
+            }
+        }
+
+    }).then(function (response) {
+        console.log(response)
+        res.json(response)
+    })
+
+})
 module.exports = router;
