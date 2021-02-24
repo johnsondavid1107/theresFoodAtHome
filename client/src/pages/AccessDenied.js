6e7c284f614a8407527947ab3c75cd73f8446e1b
Almonzo Rhoden <almonzo.rhoden@gmail.com>
2021-02-23 19:41:01 -0500

    Almonzo-AccessDenied

---
+++ client/src/pages/AccessDenied.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../pages/NoMatch.css"
+import { Col, Row, Container } from "../components/Grid";
+
+function AccessDenied() 
+{
+  return (
+    <Container>
+      <Row>
+        <Col size="lg-12">
+      <div className="center">
+
+          <br />
+          {/* Adding mobile break so it looks nice on phone - Zo */}
+          <h3 className="font-color">You have to be logged in,  <br class="mobile-break"/> <br class="mobile-break"/>
+          to do that<span> 🙅🏽‍♂️</span></h3>
+
+        <br /> 
+
+        <button type="button" class="btn btn-success" ><a href="/">Click here to login</a></button>
+        
+      </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default AccessDenied;
