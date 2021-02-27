c5e03ccbc70b7265698d6dedcb8cad9aacd99762
rquiroz3290 <rquiroz3290@gmail.com>
2021-02-27 11:55:42 -0500

    added Access Deny redirect, fixed bug where Access Denied component was rendering before check for user was happening, moved Pages/Pantry.js and Pages/Recipe.js into components, added PantryPage and RecipePage

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,11 @@ import UserProvider from "./Providers/UserProvider";
 import Application from "./components/Application/index"
 
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
-function App() {
-
+function App() {  
 
   return (
     <UserProvider>
-      <Application />
+      <Application/>
     </UserProvider>
   );
 }
