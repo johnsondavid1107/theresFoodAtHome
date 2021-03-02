74a26f9f17005feb993171c4b4a3a15bb3e9084d
Julie Skolnik <jtskolnik@gmail.com>
2021-03-02 18:52:17 -0500

    trying again heroku

--- client/src/components/Recipe/Recipes.js
+++ client/src/components/Recipe/Recipes.js
@@ -3,23 +3,23 @@ import API from "../../utils/API";
 import { Container, Row, Col, Button, Card, Accordion, Image, Form, Tab, Tabs } from "react-bootstrap";
 import UserContext from "../../utils/UserContext";
 import "./Recipe.css";
-
-
+ 
+ 
 function Recipes() {
   //Julie's JS
   //USER INFORMATION -- will be variables from firebase
   const user = useContext(UserContext)
   let userId = user.uid;
-
+ 
   //User's allergies
   const [allergies, setAllergies] = useState("");
-
+ 
   //All foods from the db as a hook 
   const [foods, setFoods] = useState([]);
-
+ 
   //The string that will go into the API search query
   const [searchTerm, setSearchTerm] = useState("");
-
+ 
   //Will become the array of objects from user data
   let foodArray = [];
   let trueFalseArray = [];
@@ -29,32 +29,32 @@ function Recipes() {
   let trueFalseArrayExpiring = [];
   let expiredArray = [];
   let trueFalseArrayExpired = [];
-
+ 
   //search results
   const [searchResults, setSearchResults] = useState([]);
-
+ 
   //Handling checkbox - an array of booleans
   const [isCheckedFresh, setIsCheckedFresh] = useState([false]);
   const [isCheckedExpiring, setIsCheckedExpiring] = useState([false]);
   const [isCheckedExpired, setIsCheckedExpired] = useState([false]);
   const [isCheckedAllergies, setIsCheckedAllergies] = useState([false, false, false]);
-
+ 
   //Different list of ingredients
   const [freshList, setFreshList] = useState([]);
   const [expiringList, setExpiringList] = useState([]);
   const [expiredList, setExpiredList] = useState([]);
-
+ 
   //Different list of search terms
   const [freshSearchTerm, setFreshSearchTerm] = useState("");
   const [expiringSearchTerm, setExpiringSearchTerm] = useState("");
   const [expiredSearchTerm, setExpiredSearchTerm] = useState("");
-
+ 
   //On start up, foods loaded from database and set to a react hook
   useEffect(() => {
     loadFoods(userId);
     setFoods(foodArray);
   }, []);
-
+ 
   //Function to load up foods from user's db 
   const loadFoods = userId => {
     API.getFoods(userId)
@@ -62,12 +62,12 @@ function Recipes() {
         console.log(res.data);
         //Makes an array of objects for each food item in pantry
         let dbObject = res.data[0].foodItem;
-
+ 
         for (let i = 0; i < dbObject.length; i++) {
-
+ 
           //get name
           let name = dbObject[i].name;
-
+ 
           //get date of purchase
           let dateOfPurchase = dbObject[i].dateOfPurchase;
           if (!dateOfPurchase) {
@@ -77,32 +77,32 @@ function Recipes() {
           }
           let location = dbObject[i].location;
           let daysFresh = dbObject[i].daysFresh;
-
+ 
           //Gets a spoil date for each item
           let spoilDate = new Date(dateOfPurchase.valueOf())
           spoilDate.setDate(spoilDate.getDate() + daysFresh);
           if (spoilDate == "Invalid Date") {
             spoilDate = "";
           }
-
-
+ 
+ 
           //Gets number of days fresh for each item - difference between today's date and the spoil date
           let today = new Date();
           let dd = String(today.getDate()).padStart(2, '0');
           let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
           let yyyy = today.getFullYear();
-
+ 
           today = mm + '/' + dd + '/' + yyyy;
-
+ 
           let total = Math.floor((new Date(spoilDate) - new Date(today)) / (1000 * 3600 * 24));
           if (isNaN(total)) {
             //If it isn't a number, assume it is nonperishable and make it 100 days by default
             total = 100;
           }
-
+ 
           //Gets id - may remove this later if not needed
           let id = dbObject[i]._id;
-
+ 
           //get remaining time - still working on this
           let foodObject = {
             id: id,
@@ -114,10 +114,10 @@ function Recipes() {
             daysRemaining: total,
             location: location
           }
-
+ 
           foodArray.push(foodObject);
           trueFalseArray.push(false);
-
+ 
           //Make fresh list
           if (daysFresh > 7) {
             freshArray.push(foodObject);
@@ -133,7 +133,7 @@ function Recipes() {
             }
           }
         }
-
+ 
         setFoods(foodArray);
         setIsCheckedFresh(trueFalseArrayFresh);
         setIsCheckedExpiring(trueFalseArrayExpiring);
@@ -141,70 +141,43 @@ function Recipes() {
         setFreshList(freshArray);
         setExpiringList(expiringArray);
         setExpiredList(expiredArray);
-
-        //On start automatically do a search for all of the foods that are going to expire in the next week
-        //List must be comma separated
-        let ingredients = "";
-        for (let i = 0; i < foods.length; i++) {
-          if (foods[i].daysRemaining >= 0 && foods[i].daysRemaining < 7) {
-            ingredients += foods[i].name + ",";
-          }
-        }
-
-        //Removes the last comma
-        ingredients = ingredients.replace(/,\s*$/, "");
-        setSearchTerm(ingredients);
-
-        if (ingredients === "") {
-          //Since no items are about to expire, show recipes for other foods
-          for (let i = 0; i < foods.length; i++) {
-            if (foods[i].daysRemaining >= 0) {
-              ingredients += foods[i].name + ",";
-            }
-            if (ingredients === "") {
-              ingredients = "eggs,onion,flour";
-            }
-          }
-          ingredients = ingredients.replace(/,\s*$/, "");
-          setSearchTerm(ingredients);
-        }
-        makeSearchTerm(searchTerm);
+ 
       })
       .catch(err => console.log(err))
   }
-
+ 
   //decides which API route to call and calls it based on ingredients as a parameter
   const makeSearchTerm = ingredients => {
     //If no special diet and no allergies
     if (allergies === "") {
       API.recipeFromIngredients(ingredients).then(res => {
-        console.log(res.data);
         setSearchResults(res.data);
       })
       //If allergies but no special diet
     } else {
       //They have a special diet AND allergies
       API.recipeAllergy(ingredients, allergies).then(res => {
-        console.log(res.data);
         setSearchResults(res.data);
         //If recipe search delivers no results, do generic search
-
+ 
       })
     }
-
+ 
     if (searchResults.length) {
       let cardObject = [
         {
           name: "Sorry, no recipes found!",
           image: "https://spoonacular.com/recipeImages/133742-312x231.jpg"
         }
-      ]
+      ];
+ 
+      console.log(cardObject);
       setSearchResults(cardObject);
     }
-
+ 
   }
-
-
+ 
+ 
   //Makes list of fresh items for user to add to search term
   const addToListFresh = event => {
     //Gets index of just the fresh list
@@ -212,15 +185,15 @@ function Recipes() {
     if (!index) {
       index = "0";
     }
-
+ 
     let freshArray = isCheckedFresh;
-
+ 
     if (freshArray[index] === false) {
       freshArray[index] = true;
     } else {
       freshArray[index] = false;
     }
-
+ 
     setIsCheckedFresh(freshArray);
     let searchList = "";
     for (let i = 0; i < freshList.length; i++) {
@@ -228,11 +201,11 @@ function Recipes() {
         searchList += freshList[i].name + ",";
       }
     }
-
+ 
     searchList = searchList.replace(/,\s*$/, "");
     setFreshSearchTerm(searchList);
   }
-
+ 
   //Makes list of expiring items for user to add to search term
   const addToListExpiring = event => {
     //Gets index of just the fresh list
@@ -240,15 +213,15 @@ function Recipes() {
     if (!index) {
       index = "0";
     }
-
+ 
     let expiringArray = isCheckedExpiring;
-
+ 
     if (expiringArray[index] === false) {
       expiringArray[index] = true;
     } else {
       expiringArray[index] = false;
     }
-
+ 
     setIsCheckedExpiring(expiringArray);
     let searchList = "";
     for (let i = 0; i < expiringList.length; i++) {
@@ -256,11 +229,11 @@ function Recipes() {
         searchList += expiringList[i].name + ",";
       }
     }
-
+ 
     searchList = searchList.replace(/,\s*$/, "");
     setExpiringSearchTerm(searchList);
   }
-
+ 
   //Makes list of expired items for user to add to search term
   const addToListExpired = event => {
     //Gets index of just the fresh list
@@ -268,15 +241,15 @@ function Recipes() {
     if (!index) {
       index = "0";
     }
-
+ 
     let expiredArray = isCheckedExpired;
-
+ 
     if (expiredArray[index] === false) {
       expiredArray[index] = true;
     } else {
       expiredArray[index] = false;
     }
-
+ 
     setIsCheckedExpired(expiredArray);
     let searchList = "";
     for (let i = 0; i < expiredList.length; i++) {
@@ -284,14 +257,14 @@ function Recipes() {
         searchList += expiredList[i].name + ",";
       }
     }
-
+ 
     searchList = searchList.replace(/,\s*$/, "");
     setExpiredSearchTerm(searchList);
   }
-
+ 
   //Makes search term and runs API upon search button being clicked
   const runAPI = () => {
-
+ 
     //Cancatanate the strings
     let searchTerm = "";
     if (freshSearchTerm) {
@@ -303,35 +276,35 @@ function Recipes() {
     if (expiredSearchTerm) {
       searchTerm += expiredSearchTerm;
     }
-
+ 
     searchTerm = searchTerm.replace(/,\s*$/, "");
-
+ 
     makeSearchTerm(searchTerm);
   }
-
+ 
   //When the image is clicked, reroutes to google search for the recipe
   const imageClicked = e => {
     let item = e.target.alt;
     window.open(`https://www.google.com/search?q=${item}`);
   }
-
+ 
   
   const setAllergiesString = event => {
     // event.preventDefault();
-
+ 
     let id = event.target.id;
     id = id.slice(id.length - 1);
     id = id - 1;
-
+ 
     let allergiesYesNo = isCheckedAllergies;
     if (allergiesYesNo[id] === false) {
       allergiesYesNo[id] = true;
     } else {
       allergiesYesNo[id] = false;
     }
-
+ 
     setIsCheckedAllergies(allergiesYesNo);
-
+ 
     let allergiesString = "";
     if (isCheckedAllergies[0] === true) {
       allergiesString += "peanuts,";
@@ -342,15 +315,15 @@ function Recipes() {
     if (isCheckedAllergies[2] === true) {
       allergiesString += "fish,";
     }
-
+ 
     allergiesString = allergiesString.replace(/,\s*$/, "");
     setAllergies(allergiesString);
   }
-
+ 
   return (
     <div>
       <Container fluid>
-
+ 
         <Card className="mt-3 text-center">
           <Card.Body>
             <Tabs className="justify-content-center" defaultActiveKey="ingredientsList" id="uncontrolled-tab-example">
@@ -448,7 +421,7 @@ function Recipes() {
             </Tabs>
           </Card.Body>
         </Card>
-
+ 
         <Container fluid className="mt-3">
           <Row>
             {searchResults.map((result, index) => {
@@ -465,13 +438,19 @@ function Recipes() {
             })}
           </Row>
         </Container>
-
-
+ 
+ 
       </Container>
     </div>
   );
 }
-
+ 
 export default Recipes;
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
